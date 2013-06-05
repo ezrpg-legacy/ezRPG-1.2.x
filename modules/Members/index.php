dffea94f8b4e7953c344a18ebff4db3cbb06f498
Tim Garrity <timgarrity89@gmail.com>
2013-06-05 01:18:32 -0400

    Fixed Member Module for players_meta

--- modules/Members/index.php
+++ modules/Members/index.php
@@ -22,7 +22,7 @@ class Module_Members extends Base_Module
         else
             $page = 0;
         
-        $query = $this->db->execute('SELECT `username`, `level` FROM `<ezrpg>players` ORDER BY `id` ASC LIMIT ?,50', array($page * 50));
+        $query = $this->db->execute('SELECT `username`, `level` FROM `<ezrpg>players` INNER JOIN `<ezrpg>players_meta` ON `pid` = `id` ORDER BY `id` ASC LIMIT ?,50', array($page * 50));
         $members = $this->db->fetchAll($query);
         
         $prevpage = (($page - 1) >= 0) ? ($page - 1) : 0;
