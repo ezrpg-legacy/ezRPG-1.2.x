1c38f0f68b9ddac8efc81378b27415314d9afc7e
uaktags <timgarrity89@gmail.com>
2013-07-13 14:23:55 -0400

    Various installer fixes for updates
    Added module_id to menus
    Extended password to 255 from 40 for BCrypt functionality
    Plugins Meta had some column name changes
    Added functionality to Core plugins for manager.

--- install/modules/Populate/index.php
+++ install/modules/Populate/index.php
@@ -28,7 +28,7 @@ class Install_Populate extends InstallerFactory
 CREATE TABLE IF NOT EXISTS `<ezrpg>players` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `username` varchar(30) default NULL,
-  `password` varchar(40) default NULL,
+  `password` varchar(255) default NULL,
   `email` varchar(255) default NULL,
   `pass_method` tinyint(3) default NULL,
   `secret_key` text,
@@ -88,6 +88,7 @@ CREATE TABLE IF NOT EXISTS `<ezrpg>menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `active` int(11) NOT NULL,
   `parent_id` int(11) NOT NULL DEFAULT '0',
+  `module_id` int(11) NOT NULL DEFAULT '0',
   `name` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `AltTitle` varchar(255) DEFAULT NULL,
@@ -106,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `<ezrpg>plugins` (
   `filename` text NOT NULL,
   `type` varchar(255) NOT NULL,
   `installed` int(2) NOT NULL,
-  `pid` int(11) NOT NULL DEFAULT '0',
+  `pid` bigint(20) NOT NULL DEFAULT '0',
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 QUERY;
@@ -119,13 +120,13 @@ CREATE TABLE IF NOT EXISTS `<ezrpg>settings` (
   `name` varchar(120) NOT NULL,
   `title` varchar(120) NOT NULL,
   `description` text NOT NULL,
-  `optionscode` text NULL,
-  `value` text NULL,
+  `optionscode` text,
+  `value` text,
   `disporder` smallint(5) NOT NULL DEFAULT '0',
   `gid` smallint(5) NOT NULL DEFAULT '0',
   `isdefault` int(1) NOT NULL DEFAULT '0',
   `visible` tinyint(1) NOT NULL DEFAULT '1',
-  UNIQUE KEY id (id)
+  UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 QUERY;
 	
@@ -145,8 +146,8 @@ QUERY;
 	
 	$structure8 = <<<QUERY
 CREATE TABLE IF NOT EXISTS `<ezrpg>plugins_meta` (
-   `id` int(11) NOT NULL,
-  `pid` int(11) NOT NULL,
+   `meta_id` int(11) NOT NULL,
+  `plug_id` int(11) NOT NULL,
   `version` float NOT NULL,
   `author` varchar(255) NOT NULL,
   `description` text NOT NULL,
@@ -158,20 +159,20 @@ QUERY;
 	$db->execute($structure8);
 	
 	$data1 = <<<QUERY
-INSERT INTO `<ezrpg>menu` (`id`, `parent_id`, `name`, `title`, `AltTitle`, `uri`, `pos`, `active`) VALUES
-(1, 0, 'UserMenu', 'User Menu',NULL, '', 0, 1),
-(2, 1, 'EventLog', 'Event Log',NULL, 'index.php?mod=EventLog', 0, 1),
-(3, 1, 'City', 'City',NULL, 'index.php?mod=City', 1, 1),
-(4, 1, 'Members', 'Members',NULL, 'index.php?mod=Members', 2, 1),
-(5, 1, 'Account', 'Account',NULL, 'index.php?mod=AccountSettings', 3, 1),
-(6, 0, 'WorldMenu', 'World Menu',NULL, '', 0, 1),
-(7, 6, 'Members', 'Members',NULL, 'index.php?mod=Members', 0, 1),
-(8, 0, 'AdminMenu', 'Admin Menu',NULL, '', 0, 1),
-(9, 8, 'Members', 'Members','Member Management', 'index.php?mod=Members', 0, 1),
-(10, 8, 'Menus', 'Menus', 'Menu Management', 'index.php?mod=Menu', 0, 1),
-(11, 8, 'Themes', 'Themes', 'Themes Management', 'index.php?mod=Themes', 0, 1),
-(12, 8, 'Settings', 'Settings', 'Settings Management', 'index.php?mod=Settings', 0, 1),
-(13, 8, 'Plugins', 'Plugins', 'Plugin Management', 'index.php?mod=Plugins', 0, 1);
+INSERT INTO `<ezrpg>menu` (`id`, `parent_id`, `name`, `title`, `AltTitle`, `uri`, `pos`, `active`, `module_id`) VALUES
+(1, 0, 'UserMenu', 'User Menu',NULL, '', 0, 1, 0),
+(2, 1, 'EventLog', 'Event Log',NULL, 'index.php?mod=EventLog', 0, 1, 3),
+(3, 1, 'City', 'City',NULL, 'index.php?mod=City', 1, 1, 4),
+(4, 1, 'Members', 'Members',NULL, 'index.php?mod=Members', 2, 1, 6),
+(5, 1, 'Account', 'Account',NULL, 'index.php?mod=AccountSettings', 3, 1, 5),
+(6, 0, 'WorldMenu', 'World Menu',NULL, '', 0, 1, 0),
+(7, 6, 'Members', 'Members',NULL, 'index.php?mod=Members', 0, 1, 6),
+(8, 0, 'AdminMenu', 'Admin Menu',NULL, '', 0, 1, 0),
+(9, 8, 'Members', 'Members','Member Management', 'index.php?mod=Members', 0, 1, 0),
+(10, 8, 'Menus', 'Menus', 'Menu Management', 'index.php?mod=Menu', 0, 1, 0),
+(11, 8, 'Themes', 'Themes', 'Themes Management', 'index.php?mod=Themes', 0, 1, 0),
+(12, 8, 'Settings', 'Settings', 'Settings Management', 'index.php?mod=Settings', 0, 1, 0),
+(13, 8, 'Plugins', 'Plugins', 'Plugin Management', 'index.php?mod=Plugins', 0, 1, 0);
 QUERY;
 
 	$db->execute($data1);
@@ -187,6 +188,32 @@ INSERT INTO `<ezrpg>settings` (`id`, `name`, `title`, `description`, `optionscod
 QUERY;
 
 	$db->execute($data2);
+	
+	$data3 = <<<QUERY
+INSERT INTO `<ezrpg>plugins` (`id`, `title`, `active`, `filename`, `type`, `installed`, `pid`) VALUES
+(1, 'ezRPGCore', 1, '', 'module', 1, 0),
+(2, 'Login', 1, '', 'module', 1, 1),
+(3, 'EventLog', 1, '', 'module', 1, 1),
+(4, 'City', 1, '', 'module', 1, 1),
+(5, 'AccountSettings', 1, '', 'module', 1, 1),
+(6, 'Members', 1, '', 'module', 1, 1),
+(7, 'Logout', 1, '', 'module', 1, 1);
+QUERY;
+
+	$db->execute($data3);
+	
+		$data4 = <<<QUERY
+INSERT INTO `<ezrpg>plugins_meta` (`meta_id`, `plug_id`, `version`, `author`, `description`, `url`, `uninstall`) VALUES
+(1, 2, 1.2, 'ezRPGTeam', 'Login Module - ezRPGCore', '', ''),
+(2, 3, 1.2, 'ezRPGTeam', 'EventLog Module - ezRPGCore', '', ''),
+(3, 4, 1.2, 'ezRPGTeam', 'City Module - ezRPGCore', '', ''),
+(4, 5, 1.2, 'ezRPGTeam', 'AccountSettings - ezRPGCore', '', ''),
+(5, 6, 1.2, 'ezRPGTeam', 'Members - ezRPGCore', '', ''),
+(6, 7, 1.2, 'ezRPGTeam', 'Logout Module - ezRPGTeam', '', ''),
+(7, 1, 1.2, 'ezRPGTeam', 'ezRPG Core. Disabling this will disable ALL core modules. Deleting with completely REMOVE ALL core modules', '', '');
+QUERY;
+
+	$db->execute($data4);
 	$this->header();
 		echo "<h2>The database has been populated.</h2>\n";
 		echo "<a href=\"index.php?step=CreateAdmin\">Continue to next step</a>";
