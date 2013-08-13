72b603d81989cee643e981a6535e7234fbaf0453
uaktags <timgarrity89@gmail.com>
2013-08-12 23:39:20 -0700

    CACHE support was added #21

--- install/index.php
+++ install/index.php
@@ -4,12 +4,14 @@ define('IN_EZRPG', true);
 define('LIB_DIR', realpath("../lib"));
 define('CUR_DIR', realpath(dirname(__FILE__)));
 define('MOD_DIR', CUR_DIR . '/modules');
+define('CACHE_DIR', '../cache/');
 define('EXT_DIR', '../lib/ext');
 
 require_once '../lib/class.base_module.php';
 require_once '../lib/exception.db.php';
 require_once '../lib/const.errors.php';
 require_once '../lib/class.dbfactory.php';
+require_once '../lib/func.modules.php';
 require_once CUR_DIR . '/lib/class.installerfactory.php';
 
 $installer = new InstallerFactory;
