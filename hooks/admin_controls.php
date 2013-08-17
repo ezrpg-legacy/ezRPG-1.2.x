4325852bee186e977e93026fea5d3182eefe2ce6
uaktags <timgarrity89@gmail.com>
2013-08-17 06:29:03 -0700

    Updated Admin_Controls hook
    Admin_controls I silently pushed is a debug system that accepts GET
    commands with the ?admin= parameter.
    3 commands are:
    flushSettings
    flushModules
    flushMenus

--- hooks/admin_controls.php
+++ hooks/admin_controls.php
@@ -7,23 +7,23 @@ $hooks->add_hook('admin_header', 'admin_controls', 1);
 
 function hook_admin_controls(&$db, &$tpl, &$player, $args = 0)
 {
-    
     if(isset($_GET['admin']))
 	{
 		if(isAdmin($player)){
 			switch($_GET['admin'])
 			{
 				case 'flushSettings':
-					$tpl->assign('MSG', 'Flushed Settings Cache!');
 					killSettingsCache();
 					break;
 				case 'flushModules':
 					killModuleCache();
 					break;
+				case 'flushMenus':
+					killMenuCache();
+					break;
 			}
 		}
 	}
-    //$tpl->assign('MSG', $status_messages);
 
     return $args;
 }
