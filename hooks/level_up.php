7ddf1b6a30ad70f275a5d5833c5a7a44cb541c6a
uaktags <timgarrity89@gmail.com>
2016-07-09 23:09:01 -0400

    I can't begin to understand why exp was set to just reset itself -_-. If the idea was so that every level has it's own new set of XP from 0-max, then that's stupid. Just have an overall xp.

--- hooks/level_up.php
+++ hooks/level_up.php
@@ -16,7 +16,7 @@ function hook_level_up($db, &$tpl, $player, $args = 0)
     if ( $args->exp >= $args->max_exp )
     {
         //Update the current player variable ($args)
-        $args->exp = $args->exp - $args->max_exp;
+        //$args->exp = $args->exp - $args->max_exp;  //Dafaq?
         $args->level += 1;
         $args->stat_points += 2;
         $args->max_exp += 20;
@@ -26,7 +26,7 @@ function hook_level_up($db, &$tpl, $player, $args = 0)
         $args->max_hp += 5;
 
         //Update the database
-        $db->execute('UPDATE `<ezrpg>players_meta` SET `exp`=?, `level`=level+1, `stat_points`=stat_points+2, `max_exp`=max_exp+20, `energy`=energy+1, `max_energy`=max_energy+1, `hp`=hp+5, `max_hp`=max_hp+5 WHERE `pid`=?', array( intval($args->exp), intval($args->id) ));
+        $db->execute('UPDATE `<ezrpg>players_meta` SET `level`=level+1, `stat_points`=stat_points+2, `max_exp`=max_exp+20, `energy`=energy+1, `max_energy`=max_energy+1, `hp`=hp+5, `max_hp`=max_hp+5 WHERE `pid`=?', array( intval($args->id) ));
         $debugTimer['UPDATE Stats'] = microtime(1);
         //Add event log
         $msg = 'You have leveled up! You gained +2 stat points and +1 max energy!';
